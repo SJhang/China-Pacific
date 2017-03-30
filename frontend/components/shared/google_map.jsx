43c395a1a5ed459ad0443cf9bdfa5dc1c105a612
Sonik Jhang <sonik.jhang@utexas.edu>
2017-03-30 06:19:43 -0500

    google map api implement

---
+++ frontend/components/shared/google_map.jsx
@@ -0,0 +1,34 @@
+import React, { component } from 'react';
+import ReactDOM from 'react-dom';
+import { withRouter } from 'react-router';
+
+const _mapOptions = {
+  center: { lat: 30.3653041, lng: -97.9507103 }, // china pacific
+  zoom: 16
+};
+
+class GoogleMap extends React.Component {
+  componentDidMount() {
+    debugger;
+    const map = this.refs.map;
+    this.map = new google.maps.Map(map, _mapOptions);
+    this.marker = new google.maps.Marker({
+      position: _mapOptions.center,
+      map: this.map,
+      animation: google.maps.Animation.BOUNCE,
+      title: "China Pacific"
+    });
+  }
+
+  componentDidUpdate() {
+    marker.setMap(this.map);
+  }
+
+  render () {
+    return (
+      <div className="contact-map" ref="map">Map</div>
+    )
+  }
+}
+
+export default withRouter(GoogleMap);
--- frontend/components/sidebar/contact.jsx
+++ frontend/components/sidebar/contact.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ContactForm from '../shared/contact_form';
-import { withGoogleMap, Marker, GoogleMap } from 'react-google-maps';
+import GoogleMap from '../shared/google_map';
+import { withGoogleMap, Marker } from 'react-google-maps';
 
 const Contact = (props) => {
-  const initGoogleMap = withGoogleMap(props => {
-    let lat = 30.3653041;
-    let lng = -97.9507103;
-    debugger;
-    return (
-      <GoogleMap
-        ref={props.onMapLoad}
-        defaultZoom={3}
-        defaultCenter={{lat, lng}}
-        onClick={props.onMapClick}>
-        {props.markers.map((marker, index) => {
-          debugger;
-          return (
-            <Marker
-              {...marker}
-              onRightClick={() => props.onMarkerRightClick(index)} />
-          )
-        })}
-      </GoogleMap>
-    )
-  });
+  // const initGoogleMap = withGoogleMap(props => {
+  //   let lat = 30.3653041;
+  //   let lng = -97.9507103;
+  //   debugger;
+  //   return (
+  //     <GoogleMap
+  //       ref={props.onMapLoad}
+  //       defaultZoom={3}
+  //       defaultCenter={{lat, lng}}
+  //       onClick={props.onMapClick}>
+  //       {props.markers.map((marker, index) => {
+  //         debugger;
+  //         return (
+  //           <Marker
+  //             {...marker}
+  //             onRightClick={() => props.onMarkerRightClick(index)} />
+  //         )
+  //       })}
+  //     </GoogleMap>
+  //   )
+  // });
 
   return (
     <div className="contact-page">
@@ -33,18 +34,7 @@ const Contact = (props) => {
         </div>
         <div className="contact-desc">
           <ContactForm />
-          <initGoogleMap
-            containerElement={
-              <div style={{height: `100%`}}/>
-            }
-            mapElement={
-              <div style={{height: `100%`}}/>
-            }
-            onMapLoad={_.noop}
-            onMapClick={_.noop}
-            markers={markers}
-            onMarkerRightClick={_.noop}/>
-          <div className="contact-map"></div>
+          <GoogleMap />
         </div>
       </div>
     </div>
