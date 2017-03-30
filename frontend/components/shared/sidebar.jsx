1e20f2a29c4ed9247f6bf5cd1c3bb125fd334549
Sonik Jhang <sonik.jhang@utexas.edu>
2017-03-29 20:51:40 -0500

    location complete

--- frontend/components/shared/sidebar.jsx
+++ frontend/components/shared/sidebar.jsx
@@ -19,7 +19,7 @@ class Sidebar extends React.Component {
   }
 
   redirectToLocation() {
-    // this.props.router.push('/location');
+    this.props.router.push('/location');
   }
 
   redirectToContact() {
@@ -30,12 +30,18 @@ class Sidebar extends React.Component {
     // this.props.router.push('/press');
   }
 
+  yelpLink() {
+    window.open('https://www.yelp.com/biz/china-pacific-austin');
+  }
+
+  facebookLink() {
+    window.open('https://www.facebook.com/Chinapacificaustin/?hc_ref=SEARCH');
+  }
   render () {
     return (
       <header className="sidebar">
         <div className="sidebar-logo">
           <a onClick={() => this.redirectToHome()}>
-            <img src="https://res.cloudinary.com/dsetpdsls/image/upload/c_scale,w_311/v1490328608/china_gate_zdxbsi.png"></img>
             <span>China Pacific</span>
             <span>Restaurant</span>
           </a>
@@ -44,7 +50,7 @@ class Sidebar extends React.Component {
         <ul className="sidebar-menu">
           <li><a className="about" onClick={() => this.redirectToAbout()}>ABOUT</a></li>
           <li><a className="menu" onClick={() => this.redirectToMenu()}>MENU</a></li>
-          <li><a className="location">LOCATION</a></li>
+          <li><a className="location" onClick={() => this.redirectToLocation()}>LOCATION</a></li>
           <li><a className="contact">CONTACT</a></li>
           <li><a className="press">PRESS</a></li>
         </ul>
@@ -52,10 +58,14 @@ class Sidebar extends React.Component {
           <a className="instagram">
             <i className="fa fa-instagram fa-2x" aria-hidden="true"></i>
           </a>
-          <a className="facebook">
+          <a
+            className="facebook"
+            onClick={() => this.facebookLink()}>
             <i className="fa fa-facebook-official fa-2x" aria-hidden="true"></i>
           </a>
-          <a className="yelp">
+          <a
+            className="yelp"
+            onClick={() => this.yelpLink()}>
             <i className="fa fa-yelp fa-2x" aria-hidden="true"></i>
           </a>
           <a className="google">
--- frontend/components/sidebar/location.jsx
+++ frontend/components/sidebar/location.jsx
@@ -2,8 +2,53 @@ import React from 'react';
 
 const Location = (props) => {
   return (
-    <div />
+    <div className="location-page">
+      <div className="location-wrap">
+        <div className="location-title">
+          <h1>LOCATION</h1>
+        </div>
+        <div className="location-image">
+          <div className="overlay"></div>
+        </div>
+        <div className="location-desc">
+          <div className="location-specifics">
+            <div className="physical-address">
+              <i className="fa fa-map-marker fa-4x" aria-hidden="true"></i>
+              <p>
+                Liberty Center<br/>
+                107 Ranch Road 620 South, suite 107<br/>
+                Lakeway, TX 78734
+              </p>
+            </div>
+            <div className="phone-number">
+              <i className="fa fa-phone fa-4x" aria-hidden="true"></i>
+              <p>
+                <a href="tel:512-266-9328">512 266 - 9328</a>
+              </p>
+            </div>
+            <div className="open-hours">
+              <i className="fa fa-clock-o fa-4x" aria-hidden="true"></i>
+              <p>
+                Hours of Operation<br/>
+                Mon – Wed : 11:00AM – 09:30PM<br/>
+                Thur & Fri : 11:00AM – 10:00PM<br/>
+                Sat  & Sun : 11:30AM – 10:00PM<br/>
+              </p>
+            </div>
+            <div className="appointments">
+              <i className="fa fa-cutlery fa-4x" aria-hidden="true"></i>
+              <p>
+                Party Room Available<br/>
+                Delivery & Takeout available<br/>
+                Daily Lunch and Dinner Specials
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
+// <img src="https://res.cloudinary.com/dsetpdsls/image/upload/v1484813999/1238036_10201991481652361_1800958299_n_edt93u.png"></img>
 
 export default Location;
