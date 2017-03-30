9ddffdcd611aec4bdb2265e3d0ee7aac79abf347
Sonik Jhang <sonik.jhang@utexas.edu>
2017-03-30 04:11:35 -0500

    google map api key updated and google map feature trial

--- frontend/components/sidebar/contact.jsx
+++ frontend/components/sidebar/contact.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 import ContactForm from '../shared/contact_form';
+import { withGoogleMap, Marker, GoogleMap } from 'react-google-maps';
 
 const Contact = (props) => {
+  const initGoogleMap = withGoogleMap(props => {
+    let lat = 30.3653041;
+    let lng = -97.9507103;
+    debugger;
+    return (
+      <GoogleMap
+        ref={props.onMapLoad}
+        defaultZoom={3}
+        defaultCenter={{lat, lng}}
+        onClick={props.onMapClick}>
+        {props.markers.map((marker, index) => {
+          debugger;
+          return (
+            <Marker
+              {...marker}
+              onRightClick={() => props.onMarkerRightClick(index)} />
+          )
+        })}
+      </GoogleMap>
+    )
+  });
+
   return (
     <div className="contact-page">
       <div className="contact-wrap">
@@ -10,6 +33,17 @@ const Contact = (props) => {
         </div>
         <div className="contact-desc">
           <ContactForm />
+          <initGoogleMap
+            containerElement={
+              <div style={{height: `100%`}}/>
+            }
+            mapElement={
+              <div style={{height: `100%`}}/>
+            }
+            onMapLoad={_.noop}
+            onMapClick={_.noop}
+            markers={markers}
+            onMarkerRightClick={_.noop}/>
           <div className="contact-map"></div>
         </div>
       </div>
