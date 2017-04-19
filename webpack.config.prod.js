ba3d07cde30fe528ba27dde3a06a27a873fab8e2
SONIK JHANG <sonikjhang@SONIKs-MacBook-Pro.local>
2017-04-19 13:37:07 -0500

    webpack cofig

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -10,7 +10,7 @@ module.exports = {
   plugins:[
     new webpack.DefinePlugin({
       'process.env':{
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development' || 'production')
+        'NODE_ENV': JSON.stringify('production')
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
